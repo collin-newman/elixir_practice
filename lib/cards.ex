3caa1633b1e01466ea4d02dc97e0670e2678266e
Collin Newman <collin.newman37@gmail.com>
2021-04-18 22:31:35 -0400

    Added ex_docs and created basic cards docs

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,9 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling hands of cards
+
+  """
+
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Clubs", "Hearts", "Spades", "Diamonds"]
@@ -6,7 +11,6 @@ defmodule Cards do
     for suit <- suits, value <- values do
       "#{value} of #{suit}"
     end
-
   end
 
   def shuffle(deck) do
